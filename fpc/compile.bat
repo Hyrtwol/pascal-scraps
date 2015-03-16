b52cd12733064d6244383b70adb92037bb066ed4
Thomas la Cour <tlc@247e.com>
2015-03-16 14:21:05 +0100

    verify output

--- fpc/compile.bat
+++ fpc/compile.bat
@@ -1,4 +1,15 @@
+@echo off
 del *.o
 del *.exe
 fpc -Twin32 -Mobjfpc -O3 hello.pas
-hello.exe
+if errorlevel 1 goto ERROR
+hello.exe > hello.out
+fc hello.cmp hello.out
+if errorlevel 1 goto ERROR
+goto DONE
+:ERROR
+echo.
+echo -= ERROR =-
+echo.
+@pause
+:DONE
